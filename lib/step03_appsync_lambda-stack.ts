2b39492214396cab6ba70027ba119bd0c62ef8db
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-03 10:49:16 +0500

    AppSync Lambda Stack

--- lib/step03_appsync_lambda-stack.ts
+++ lib/step03_appsync_lambda-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Step03AppsyncLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // AppSync API
+    const api = new appsync.GraphqlApi(this, "Api", {
+      name: "cdk-appsync-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+    });
+
+    // AWS Lambda
+    const myLambda = new lambda.Function(this, "MyLambdaFunction", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "welcome.handler",
+    });
+
+    // Lambda DataSource
+    const lambdaDataSource = api.addLambdaDataSource(
+      "MyLambdaDataSource",
+      myLambda
+    );
+
+    // resolvers
+    lambdaDataSource.createResolver({
+      typeName: "Query",
+      fieldName: "welcome",
+    });
   }
 }
