502a106751809464020c4d61c43c9508a71fb597
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-03 11:27:00 +0500

    AppSyncEvent

--- lambda/welcome.ts
+++ lambda/welcome.ts
@@ -1,9 +1,15 @@
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Context,
-} from "aws-lambda";
+type AppSyncEvent = {
+  info: {
+    fieldName: String;
+  };
+};
 
-export async function handler(event: APIGatewayProxyEvent, context: Context) {
-  return "welcome";
+export async function handler(event: AppSyncEvent) {
+  if (event.info.fieldName == "welcome") {
+    return "Welcome from AppSync Lambda";
+  } else if (event.info.fieldName == "hello") {
+    return "Hello World from AppSync Lambda";
+  } else {
+    return "Not Found!";
+  }
 }
